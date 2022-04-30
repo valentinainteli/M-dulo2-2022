625aac6ac231f1afbcb54774918a3674fb6c6ae7
valentinainteli <99206842+valentinainteli@users.noreply.github.com>
2022-04-29 21:09:09 -0300

    Mais alterações

--- 03_AUT_EST_ENTREGA/Semana 2/app.js
+++ 03_AUT_EST_ENTREGA/Semana 2/app.js
@@ -1,10 +1,8 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
-
 const hostname = '127.0.0.1';
-const port = 3015;
-app.use(express.static("public"));
-
+const port = 3022;
+app.use(express.static("curriculo"));
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
